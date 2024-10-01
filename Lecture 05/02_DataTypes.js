a761836be27b463f5e52ea08eff96dc5f65aa84c
Noor Fatima <129104494+NoorFatima-developer@users.noreply.github.com>
2024-10-02 00:47:09 +0500

    file is added

---
+++ Lecture 05/02_DataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all JS code as newer version 
+
+// alert ( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+    3       //code readability should be high
+);
+
+console.log("Noor Fatima");
+
+let age = 13
+
+//Two documnetaion must know coz we will read and then use:
+
+//1---tc39
+//2---mdn
+
+
+//NOTES:
+
+// number => 2 to power 53 // example let age = 18
+// bigint
+// string = " "
+// boolean => true/false  // example let isLoggedIn = false/true
+// null => standalone value 
+// undefined => it means value is not assigned till now... //example of undefined is let state;(value is not defined so it will be undefined)
+// symbol => uniqueness(mostly used in react)
+
+
+//Object 
+console.log(typeof "Noor Fatima");          //string
+console.log(typeof age);                    //undefined
+console.log(typeof null);                   // object

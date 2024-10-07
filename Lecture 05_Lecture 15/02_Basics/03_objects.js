78767cf29d9afc42a858d3fe7afebb358d197191
Noor Fatima <129104494+NoorFatima-developer@users.noreply.github.com>
2024-10-07 14:58:27 +0500

    code is added

---
+++ Lecture 05_Lecture 15/02_Basics/03_objects.js
@@ -0,0 +1,64 @@
+// Object declare two ways:
+// Literals and Constructors...
+
+//singleton:
+
+//When we declare as a literals then singleton nahi bnta hai...
+//constructor sy aghr bnyga tu han singleton bnyga...
+
+
+
+//object literals
+
+// object.create---->constructor method..(but we will discuss later)
+//This is an object....nad object k andr keys and values ka chkr hota hai... but array k index hoty hain jesy 0 index 1 index.. 
+
+//Symbol data-types...
+
+const mysymb = Symbol("key1")
+
+const JsUser = {    
+    name: "noor",       //name is also string as well...
+    "fullname": "noorfatima",
+    age: 18,
+    location: "Lahore",
+    email:"noorfatima@gmail.com",
+    isLoggedIn: false,
+    lastLoggedIn: ["Monday", "Tuesday"],
+    [mysymb]: "mykey1"      //also usable but its good to use in [] but can practice without []
+}
+
+console.log(JsUser.email);          //right but not useable
+console.log(JsUser["email"]);       //perfect way...
+
+console.log(JsUser["fullname"]);
+console.log(JsUser[mysymb]);
+
+//redeclare object:
+
+JsUser.email = "nahfatima@gmail.com"
+
+//Freeze object so now i cannot see the next updated email...
+Object.freeze(JsUser)
+JsUser.email = "ahhhhlasjdbm"   ////like it will 
+console.log(JsUser);            
+
+//=======Function===============//
+
+
+//Once I will commen whole code then it will work:
+//with greeting and without greeting...
+
+JsUser.greeting = function(){
+    console.log("Hello JS User...");    
+}
+
+JsUser.greetingtwo = function(){
+    console.log(`Hello JS user , ${this.name}`);         //String interpretaion wo write in back-ticks..and ye jo this h islye use kea h k aghr kisi obj ko refer krna ho tu this ksath object name likhny sy wo oss obj ki properties ko show krdeta hai...
+}
+    
+// console.log(JsUser.greeting);           //output = function(anonymous)
+// console.log(JsUser.greeting());         //output = Hello JS User...
+// console.log(JsUser.greetingtwo());      //output = Hello JS User, noor
+
+
---
+++ Lecture 05_Lecture 15/02_Basics/04_objects.js
@@ -0,0 +1,94 @@
+//object with constructors...
+
+
+// //non-singleton object:(literals)
+// const tinderuser1 = {}
+// console.log(tinderuser1);        //output = {}
+    
+//singleton object:(construtor)
+
+//Example 01:
+const tinderuser = new Object()
+console.log(tinderuser);            //output = {}
+
+tinderuser.id = "123abc"
+tinderuser.name = "Sammy"
+tinderuser.loggedIn = false
+
+// console.log(tinderuser);        //outout = { id: '123abc', name: 'Sammy', loggedIn: false }
+
+
+//Example 02:(Nested objects)
+
+const regularUser = {
+
+   email: "nahfatima@gmail.com",
+   fullname : {
+    userfullname : {
+        firstname : "noor",
+        lastname : "fatima"
+    }
+   }
+}
+
+console.log(regularUser);          //output = { email: 'nahfatima@gmail.com', fullname: { userfullname: { firstname: 'noor', lastname: 'fatima' } } }
+console.log(regularUser.fullname?.userfullname.firstname);   //output = noor,, question mark is basically behaving as an if else condition and and its called ternary operator...
+
+
+
+//Example 03:
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj3 = {5: "e", 6: "f"}
+
+
+//This giving me the output of all objects but I want whole output in single object so i will not di it like this:
+const obj4 = { obj1, obj2, obj3}
+console.log(obj4);
+
+//This statement will give me output be in one object:but its not most usable object...
+const obj5 = Object.assign({}, obj1, obj2, obj3)
+console.log(obj5);          //output = { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
+
+//Spread operator is perfect and most suitable and recent method so I will use this:
+
+const obj6 = {...obj1, ...obj2, ...obj3}
+console.log(obj6);          //output = { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
+
+
+//Example 04:
+//array m bht sary objects.. jesy object m object hoskty hain essy e array m b bht sary objects hoskty hain...
+ const users = [
+    {
+        id: 1,
+        name: "John Doe"
+    },
+    {
+        id: 2,
+        name: "John Doe"
+    },
+    {
+        id: 3,
+        name: "John Doe"
+    },
+    {
+        id: 4,
+        name: "Jane Doe"
+    }
+]
+
+users[1].email
+console.log(tinderuser);
+console.log(Object.keys(tinderuser));       //result in array form
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+
+//Example 05:
+
+//to check is this value exist or not...
+console.log(tinderuser.hasOwnProperty("loggedIn"));     //output = true
+console.log(tinderuser.hasOwnProperty("isloggedIn"));   //output = false
+
+
+

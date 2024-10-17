e04bf4a216a7bf24d86f49570760f5a556b614fc
Noor Fatima <129104494+NoorFatima-developer@users.noreply.github.com>
2024-10-18 02:14:06 +0500

    code is added

--- Lecture 26_Lecture 29/Lecture 27_Lecture 29/05_Iterations/08_eight.js
+++ Lecture 26_Lecture 29/Lecture 27_Lecture 29/05_Iterations/08_eight.js
@@ -3,6 +3,8 @@
 //accumulator k andr initial value ati hai, and initial value zror deni hoti hai essy osy pta kesy chlyga k start kaha sy krna hai,,,
 //and cuurval mai current value jygi
 //mtlb pehly variable k andr initial value ayegi and other variable k andr current value aygi...
+
+//Example 01:
 const myNumbers = [1, 2, 3]
 
 const myTotal = myNumbers.reduce( function (acc, currval){
@@ -29,3 +31,37 @@ console.log(Total);
 //lkin whi bt code zda short more readble ismai lgta islye hmny ye reduce use kea...
 
 //Example 02:
+
+const ShoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+
+    {
+        itemName: "python course",
+        price: 2500
+    },
+
+    {
+        itemName: "java course",
+        price: 3500
+    },
+
+    {
+        itemName: "ruby course",
+        price: 2700
+    },
+
+    {
+        itemName: "swift course",
+        price: 3000
+    }
+]
+
+
+//yahan item basically hmara accumulator hai jismai hmri initial value ari hai or cuuval hmri current value hai jismai array mai jo object hai price ki wo ari hai...
+
+const totalShopping = ShoppingCart.reduce( (item, cuuval) => (item + cuuval.price), 0)
+
+console.log(totalShopping);

f1bf65c970c67cc423ed04d3a46e4789ac7526ba
Noor Fatima <129104494+NoorFatima-developer@users.noreply.github.com>
2024-10-18 01:13:58 +0500

    code is added

--- Lecture 26_Lecture 29/Lecture 27_Lecture 29/05_Iterations/07_seven.js
+++ Lecture 26_Lecture 29/Lecture 27_Lecture 29/05_Iterations/07_seven.js
@@ -5,3 +5,16 @@ const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const newNums = myNumbers.map( (num) => num + 10)
 console.log(newNums);
+
+
+//ye kam mai forEach sy b krskti o like this:
+
+newNum = []
+
+myNumbers.forEach( (num) => 
+   {
+    if (num >= 1) {
+        newNum.push(num + 10)
+    }
+   })
+console.log(newNum);

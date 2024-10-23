dd8307491bee0a5e460eb9f3b58a391c98f651ff
Noor Fatima <129104494+NoorFatima-developer@users.noreply.github.com>
2024-10-24 01:09:01 +0500

    code is added

---
+++ Lecture 35(DOM Projects)/Lecture 35/01_ColorChanger/chaiaurcode.js
@@ -0,0 +1,12 @@
+const body = document.querySelector('body');
+console.log(body);
+
+const buttons = document.querySelectorAll('button');
+console.log(buttons);
+
+// Nodelist k oper hum foreach(arrays k opr laga skty hain and Nodelist array ki trhan behave krta hai isley laga skty hain) 
+//laga skty hain,, lkin html collection ko pehly Nodelist mm krna hota and then hum ospr Foreach laga
+// skty hain...
+
+buttons.forEach(function(button) {
+    })

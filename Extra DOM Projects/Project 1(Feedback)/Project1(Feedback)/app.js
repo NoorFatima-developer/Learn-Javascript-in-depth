edc3cfcec6229d04a2f9a5c18490239fa615d991
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-10-29 15:58:49 +0500

    code is added

--- Extra DOM Projects/Project 1(Feedback)/Project1(Feedback)/app.js
+++ Extra DOM Projects/Project 1(Feedback)/Project1(Feedback)/app.js
@@ -1,35 +1,35 @@
-const ratings = document.querySelectorAll(".ratings");
-const ratingsContainer = document.querySelector(".ratings-container");
-const panel = document.querySelector("#panel");
-const sendBtn = document.querySelector("#send");
+// const ratings = document.querySelectorAll(".ratings");
+// const ratingsContainer = document.querySelector(".ratings-container");
+// const panel = document.querySelector("#panel");
+// const sendBtn = document.querySelector("#send");
 
 
-const selectedRating = "Satisfied";
+// const selectedRating = "Satisfied";
 
 
-ratingsContainer.addEventListener("click", (e) => {
-  if(e.target.parentNode.classList.contains("rating")) {
+// ratingsContainer.addEventListener("click", (e) => {
+//   if(e.target.parentNode.classList.contains("rating")) {
 
-    removeActive();
+//     removeActive();
 
-    e.target.parentNode.classList.add("active");
-    selectedRating = e.target.nextElementSibling.innerHTML;
-  }
-})
-
-
-sendBtn.addEventListener("click", (e)=> {
-  panel.innerHTML = `
-  <p class = "heart">❤️</p>
-  <strong>Thank You!</strong>
-  <br>
-  <strong>Feedback: ${selectedRating}</strong>
-  `
-})
+//     e.target.parentNode.classList.add("active");
+//     selectedRating = e.target.nextElementSibling.innerHTML;
+//   }
+// })
 
 
-function removeActive() {
-  for(let i = 0; i <ratings.length; i++){
-    ratings[i].classList.remove("active")
-  }
-}
+// sendBtn.addEventListener("click", (e)=> {
+//   panel.innerHTML = `
+//   <p class = "heart">❤️</p>
+//   <strong>Thank You!</strong>
+//   <br>
+//   <strong>Feedback: ${selectedRating}</strong>
+//   `
+// })
+
+
+// function removeActive() {
+//   for(let i = 0; i <ratings.length; i++){
+//     ratings[i].classList.remove("active")
+//   }
+// }

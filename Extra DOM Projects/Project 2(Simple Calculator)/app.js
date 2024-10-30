45ffc5d2c9cfa4b5d657d99c9700ec021cd74a5f
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-10-30 18:19:01 +0500

    code is added

--- Extra DOM Projects/Project 2(Simple Calculator)/app.js
+++ Extra DOM Projects/Project 2(Simple Calculator)/app.js
@@ -1,32 +1,32 @@
-// document.getElementById("btn").addEventListener("click", (e)=> {
+const result = document.querySelector('.result')
+const sbtbtn = document.querySelector('.submit')
 
-//     let num1 = document.querySelector(".num1").value;
-  
-//     let num2 = document.querySelector(".num2").value;
-  
-//     let result = document.querySelector(".result");
-  
-//     let operator = document.getElementById("selectOp").value;
-  
-  
-//   switch(operator) {
-  
-//     case "plus":
-  
-//     //result.innerHTML = Number(num1) + Number(num2);
-//       result.innerHTML = +num1 + +num2;
-//       break;
-  
-//     case "min":
-//       result.innerHTML = +num1 - +num2;
-//       break;
+sbtbtn.addEventListener('click', (e) => {
+e.preventDefault()
+const selecopt = document.querySelector('.selecopt').value;
+const num1 = document.querySelector('.num1').value;
+const num2 = document.querySelector('.num2').value;
+
+  switch(selecopt){
+
+    case "plus":
+      result.innerHTML = Number(num1) +  Number(num2);
+      break;
     
-//     case "dev":
-//       result.innerHTML = +num1 /  +num2;
-//       break;
+    case "minus":
+      result.innerHTML = Number(num1) -  Number(num2);
+      break;
+
+    case "multiply":
+      result.innerHTML = Number(num1) *  Number(num2);
+      break;
     
-//     case "multi":
-//       result.innerHTML = +num1 * +num2;
-//   }
-  
-//   });
+    case "divide":
+      if(Number2(num2) === 0){
+        result.innerHTML = "Error: Division by zero";
+      } else {
+        result.innerHTML = Number1(num1) / Number2(num2);
+      }
+ 
+  }
+})

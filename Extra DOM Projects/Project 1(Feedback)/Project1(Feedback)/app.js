ee1219f9414abaaa92cb7b3d63eb9025bd6dca35
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-03 22:56:02 +0500

    code is added

--- Extra DOM Projects/Project 1(Feedback)/Project1(Feedback)/app.js
+++ Extra DOM Projects/Project 1(Feedback)/Project1(Feedback)/app.js
@@ -41,18 +41,33 @@ const icon = document.querySelector('.icon');
 const btn = document.querySelector('.btn');
 const updatedcontainer = document.querySelector('.container');
 
-let selectedRating = "Satisfied"
+const ratingcontainer = document.querySelector('.ratingcontainer');
+
+const selectedRating = "Satisfied"
+
+
+ratingcontainer.addEventListener('click', (e) => {
+  if(e.target.parentNode.classList.contains('rating'));
+
+  removeActive();   //iska mtlb ye h k bs oss class ko active kro jo user select kry bakio ko remove krdo
+
+  e.target.parentNode.classList.add('active');
+  selectedRating = e.target.nextElementSibling.innerHTML;
+  
+});
 
 
 btn.addEventListener('click', () => {
   updatedcontainer.innerHTML = `
-  <p class="heart">��️</p>
+  <p class="heart">❤️</p>
   <strong>Thank You!</strong>
   <br />
-  <strong>Feedback: ${selectedRating}</strong>
+  <strong>Feedback: ${selectedRating}</strong>`
+});
+
+
+function removeActive() {
+  for(i = 0; i <rating.length; i++) {
+    rating[i].classList.remove('active');
+  }
 }
-
-
-
-
-

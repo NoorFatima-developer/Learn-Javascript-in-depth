03fa7157c56133e6b5509fd6e6eb268fc685f5cd
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-03 21:56:53 +0500

    code is added

--- Extra DOM Projects/Project 1(Feedback)/Project1(Feedback)/app.js
+++ Extra DOM Projects/Project 1(Feedback)/Project1(Feedback)/app.js
@@ -32,3 +32,27 @@
 
 
 
+
+
+
+
+const rating = document.querySelector('.rating');
+const icon = document.querySelector('.icon');
+const btn = document.querySelector('.btn');
+const updatedcontainer = document.querySelector('.container');
+
+let selectedRating = "Satisfied"
+
+
+btn.addEventListener('click', () => {
+  updatedcontainer.innerHTML = `
+  <p class="heart">��️</p>
+  <strong>Thank You!</strong>
+  <br />
+  <strong>Feedback: ${selectedRating}</strong>
+}
+
+
+
+
+

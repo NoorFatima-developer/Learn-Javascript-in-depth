0893a408154929b4f90a022f4d167980f2bb6824
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-03 21:22:04 +0500

    code is added

--- Extra DOM Projects/Project 1(Feedback)/Project1(Feedback)/app.js
+++ Extra DOM Projects/Project 1(Feedback)/Project1(Feedback)/app.js
@@ -1,32 +1,34 @@
-const updatedcontainer = document.querySelector('.container')
-const ratingcontainer = document.querySelector('.ratingcontainer')
-// const panel = document.querySelector('#panel');
-const rating = document.querySelectorAll('.rating');
-const button = document.querySelector('.btn')
+// const updatedcontainer = document.querySelector('.container')
+// const ratingcontainer = document.querySelector('.ratingcontainer')
+// // const panel = document.querySelector('#panel');
+// const rating = document.querySelectorAll('.rating');
+// const button = document.querySelector('.btn')
 
-const selectedRating = "Satisfied";
+// const selectedRating = "Satisfied";
 
-ratingcontainer.addEventListener("click", (e) => {
-  if(e.target.parentNode.classList.contains("rating")) {
+// ratingcontainer.addEventListener("click", (e) => {
+//   if(e.target.parentNode.classList.contains("rating")) {
 
-    removeActive();
+//     removeActive();
 
-    e.target.parentNode.classList.add("active");
-    selectedRating = e.target.nextElementSibling.innerHTML;
-  }
-})
+//     e.target.parentNode.classList.add("active");
+//     selectedRating = e.target.nextElementSibling.innerHTML;
+//   }
+// })
 
-button.addEventListener('click', function (e) {
-  updatedcontainer.innerHTML = `
-  <p class = "heart">❤️</p>
-  <strong>Thank You!</strong>
-  <br />
-  <strong>Feedback: ${selectedRating}</strong>`
-})
+// button.addEventListener('click', function (e) {
+//   updatedcontainer.innerHTML = `
+//   <p class = "heart">❤️</p>
+//   <strong>Thank You!</strong>
+//   <br />
+//   <strong>Feedback: ${selectedRating}</strong>`
+// })
 
-function removeActive(){
-  for(let i = 0; i<rating.length; i++){
-    rating[i].classList.remove('active');
-  }
-}
+// function removeActive(){
+//   for(let i = 0; i<rating.length; i++){
+//     rating[i].classList.remove('active');
+//   }
+// }
+
+
 

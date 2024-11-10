f5a2117aa0f5f3826a97482729b45d6959f45550
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-11 03:54:42 +0500

    code is added

--- Lecture 43(Prototypes)/Object.js
+++ Lecture 43(Prototypes)/Object.js
@@ -12,4 +12,8 @@ multiplyBy5.power = 2;
 
 console.log(multiplyBy5(5));        //output = 25
 console.log(multiplyBy5.power);     ////outout = 2 (iska mtlb ye h k function b 1 object e hai ku k meny isko access krlea hai)
-console.log(multiplyBy5.prototype); ////output = {} (object e aya hai output mai)
+console.log(multiplyBy5.prototype); ////output = {} (empty object e aya hai output mai)
+
+
+// So 1 bt tu clear hogi h at the end of the day everything is object in javascript...
+// mtlb function jo h function tu h  apny andr lkin zrort prny pr wo 1 object ki trhan b behave krta hai...

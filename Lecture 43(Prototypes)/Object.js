49f7b5fb362c501d5376566aec04cdc1eee374f9
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-11 04:24:48 +0500

    code is added

--- Lecture 43(Prototypes)/Object.js
+++ Lecture 43(Prototypes)/Object.js
@@ -1,4 +1,7 @@
 // Iss mai hum ye disucc krygy
+
+
+//  ==== QUESTION 01 ===
 // <!-- "Function" k andr ====> Object???(kea function b 1 object hai???) -->
 
 
@@ -18,7 +21,11 @@ console.log(multiplyBy5.prototype); ////output = {} (empty object e aya hai outp
 // So 1 bt tu clear hogi h at the end of the day everything is object in javascript...
 // mtlb function jo h function tu h  apny andr lkin zrort prny pr wo 1 object ki trhan b behave krta hai...
 
-// ====Now Again dicuss "this" keywrord in function====...
+
+//  ==== QUESTION 02 ===
+
+// ======Now Again dicuss "this" keywrord in function and Significance of "new keyword"====...
+
 // "this" keyword in simple words is k jisny b bulaya hai osk pass hi jao...
 
 function createuser(username, score){
@@ -27,9 +34,32 @@ function createuser(username, score){
     this.score = score;
 }
 
+// ab aghr m score++ k sath "this" keyword ni lagaogi tu osko pata nahi lgy ga k user1 ko ++ krna hai ya user2 ko...
+// so this lagany k bd ye bt kafi hd tk clear o jygi lkin abi b pura kam ni hoa..
+
+// And 1 or baat ye hai k jo prototype keyword lgya hai na ye smjany jlye lgya hai k behind the seen kesy hum objects ko access krskty hain by using
+// prototype .. ab object k andr aghr hum console m dekhain tu alot of objects hoty hain...
+// mnz ye k prototype k andr prototype phr prototype..tu ab hum kitny prototypes func k sath likhy islye by-default behaviour m hum 1 b nahi likhty or kam chl jata hai...
+
 createuser.prototype.increment = function(){
     this.score++;
 }
 
-const user1 =  createuser("John Doe", 100);
-const user2 =  createuser("John Raye", 200);
+// ye function basically increment krny klye use kr rye hain...
+
+createuser.prototype.printMe = function(){
+    console.log(`Price is: ${this.score}`);
+}
+
+console.log(createuser);
+
+// ab this tu oper use krlea tha lkin osko konsa this? user1 ka ya user2 ka this? tu islye this keyword k sath "new" keyword utana e important hai...
+const user1 =  new createuser("John Doe", 100);
+const user2 =  new createuser("John Raye", 200);
+
+console.log(user1);
+console.log(user2);
+
+user1.printMe();
+user2.printMe();
+
--- Lecture 43(Prototypes)/notes.md
+++ Lecture 43(Prototypes)/notes.md
@@ -2,6 +2,9 @@
 
 ## And one important thing is that k javascript ka default behaviour hai prototypal behaviour...iska mtlb ye h k javascript kbi b har nahi manti... ap oski koi b chez dekhna chahty o tu wo oper ja k dekhti fer or oper ja k dekhti hai...mnz wo parents ko access krti hai , grand parents ko access krti hai lkin har nahi manti...jbtk k osko "null value" na mil jye..tu yehi javascript ka prototypal behaviour hai... isi behaviour sy link o k "hamara this keyword krta hai", and arrow function mai "this keyword ka accs nahi hota hai ku k osk andr protypal behaviour ajta hai that's why..." classes iski wjaa sy mili hain, this keyword b isk through hi kam kr rhai hai..or jo prototypal inheritance jo h wo inheritance k andr wo b issi "prototypal behaviour" ki waja sy hi araha hai...
 
+<!-- IMPORTANT BAAT: -->
+## Javascript classes k through constructor function nahi deti balky javascript "new keyword" k through constructor function deti hai...
+
 <!-- Javascript kabi b har nahi manti... iski ko e hum inheritance b bolty hain mnz prototypal inheritance... -->
 
 <!-- Javascript k andr hr chez hi apny apmai 1 object hai...

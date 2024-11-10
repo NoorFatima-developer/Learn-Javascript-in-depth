cba3bc4e33bd65f3b46256b2cd3ae5be27d09093
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-11 04:04:49 +0500

    code is added

--- Lecture 43(Prototypes)/Object.js
+++ Lecture 43(Prototypes)/Object.js
@@ -19,8 +19,17 @@ console.log(multiplyBy5.prototype); ////output = {} (empty object e aya hai outp
 // mtlb function jo h function tu h  apny andr lkin zrort prny pr wo 1 object ki trhan b behave krta hai...
 
 // ====Now Again dicuss "this" keywrord in function====...
+// "this" keyword in simple words is k jisny b bulaya hai osk pass hi jao...
 
 function createuser(username, score){
+    // yahan tu "this" keyword differentiate krny kley likha hai k konsa variable hai and konsi value hai...
     this.username = username;
     this.score = score;
 }
+
+createuser.prototype.increment = function(){
+    this.score++;
+}
+
+const user1 =  createuser("John Doe", 100);
+const user2 =  createuser("John Raye", 200);

ef1c34b0daa585b5747b7e0710038398b742ce65
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-11 03:51:52 +0500

    code is added

--- Lecture 43(Prototypes)/Object.js
+++ Lecture 43(Prototypes)/Object.js
@@ -8,4 +8,8 @@ function multiplyBy5(num){
     return num*5;
 }
 
-console.log(multiplyBy5(5));
+multiplyBy5.power = 2;
+
+console.log(multiplyBy5(5));        //output = 25
+console.log(multiplyBy5.power);     ////outout = 2 (iska mtlb ye h k function b 1 object e hai ku k meny isko access krlea hai)
+console.log(multiplyBy5.prototype); ////output = {} (object e aya hai output mai)

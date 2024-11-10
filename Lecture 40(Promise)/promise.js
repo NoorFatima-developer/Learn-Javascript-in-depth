b569c13eabd1936a35a687e0ded6c159fce01e82
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-10 22:54:01 +0500

    code is added

--- Lecture 40(Promise)/promise.js
+++ Lecture 40(Promise)/promise.js
@@ -1,4 +1,4 @@
-// promise smjo esa e h k hmsy yr promise krta h k ye hmra kam krdyga but wait krta h k kuch time tk kryga jldi b late b p skta etc..
+// promise smjo esa e h k hmsy ye promise krta h k ye hmra kam krdyga but wait krta h k kuch time tk kryga jldi b krskta hai or late b kr skta etc..
 // promises ko hum create b krty hain and consume b krty hain but zda tr esi examples hongi jinmai hum log promises ko consume krty hain..
 
 // Example:(Hum promises ko kesy consume krskty hain...)
@@ -6,10 +6,11 @@
 // then m response ata hai, catch m error ata hai and finally mai sb kuch ata h ku k finally always execute...
 // fetch('https://api.com').then().catch().finally()   //essy hum consume krty hain promises ko...
 
-//lkin creae krna b tu sikhna hai so see at below:
+//lkin create krna b tu sikhna hai so see at below:
 // promise apny andr callback leta hai blky callback hell(callback hell ka mtlb ye h k callback k andr e callback fer callback)
 
-// NOTE: Basically reolve directly connected hmry then k andr...islye tu hum then ko consume krny k bd resolve krty hain setTimeout k andr...
+// NOTE: Basically resolve directly connected hai hmry then k andr...islye tu hum then ko consume krny k bd resolve krty hain setTimeout k andr...
+
 // ==================================Promise Practice 01 Task======================================
 //1---Creation Code of promises:(by storing in variable)
 
@@ -19,9 +20,10 @@ const promiseOne = new Promise(function(resolve, reject){
 
     setTimeout(() => {
         console.log('Async task is completed');
-        // jb thk m rsolve wala func run ni krogi tbtk wo resolve ni hoga...and tbtk "Promise comleted screen pr shwow ni oga"
+        // jb thk m rsolve wala func run ni krogi tbtk wo resolve ni hoga...and tbtk "Promise comleted screen pr shwow ni hoga"
         //if task is successful
-        // 3--promise reolve
+
+        // 3--Promise reolve
         resolve('Task One Successful');
     }, 2000);
 })
@@ -90,16 +92,15 @@ const promiseFour = new Promise(function(resolve, reject){
     }, 1000)
 })
 
-// 2-- Consume Promise(reolve and rehect jahan use krty hain osko consume krny ka different way hota hai)
+// 2-- Consume Promise(reolve and reject jahan use krty hain osko consume krny ka different way hota hai)
 
 // pehli chez ye h k jb hum reject use krty hain tu store time hum osko 1 variable m store krk log ni krwaty skty..
 // jesy yahan noor h...
 
 // const noor = promiseFour.then(function(user){
-//     console.log("Promise 4 resolved");
+//     console.log("Promise 4 resolved", data);
 // })
-
-// console.log(noor);
+// console.log(noor);   //Error not worked....
 
 // Ab ye dekhygy kesy consume kry hum..(and kesy hum callback hell sy bach skty hain using then and catch...callback hell ye hota hai jismai hmry ps alot of callbacks hoti h)
 // chaining of .then....
@@ -134,12 +135,12 @@ const promiseFive = new Promise(function(resolve, reject){
         }
     }, 1000)
 })
-// 2--Concsume Promise:
+// 2--Concume Promise:
 // Async b tu wait krta h and then kam krta hai(Async blocking in js m e tu ata hai)
 // so we can consume it as async as well ku k promise b wait krta hai and thne kam krta hai same essy e async b tu wait krta hai and then kam krta hai
 
 
-// async directly errors ko handle ni hoty so use try catch ya .then catch..
+// async directly errors ko handle ni krty so use try catch with async or  i can also use catch here as well.
 async function consumepromisefile(){
     // promise hmry ps 1 object h islye hum osko essy consume ni krygy jesy meny below m kea h...
     // await promiseFive()    
@@ -184,3 +185,6 @@ fetch('https://jsonplaceholder.typicode.com/users')
 })
 .catch((error) => 
       {return console.log(error)});
+
+// NOTE:
+// Ab important bt ye h k try, catch jo h wo hmesha async func k andr e run o ga and .then, catch without async func hi run hoga...

d51bd04f73e487360880bc1ac9bf88633687aec1
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-11 20:09:19 +0500

    code is added

--- Lecture 44_Lecture 46/Lecture 44(Call & Bind)/call.js
+++ Lecture 44_Lecture 46/Lecture 44(Call & Bind)/call.js
@@ -1,4 +1,5 @@
 // ye username smjo US m define hoa hai
+
 function SetUsername(username) {
     // DB Complex calls
     this.username = username;
@@ -7,9 +8,15 @@ function SetUsername(username) {
 }
 
 // email and password pakistan m define hoey hain
+// ab m US waly ko pakistan m define krna chahti hon..
 function createUser(username, email, password) {
 
-    SetUsername.call(username);
+    // ====Call ki kahani?===(In simple words, current execution context kisi aur function ko pass krdeta hai...)
+    // US ka this basically apna current execution context Pakistan k function ko pass kerrha hai..ye hai call
+    // jismai 1 func apna this dosry function ko dedey k me iss chor rha ho ab tm snbalo...
+
+    SetUsername.call(this, username);
+
     this.email = email;
     this.password = password;
 }

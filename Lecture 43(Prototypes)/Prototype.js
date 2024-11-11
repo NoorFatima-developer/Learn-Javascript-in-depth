6bf3387cc14395f9e1f65ae5a0a939bcf4ea2bac
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-11 05:14:48 +0500

    code is added

---
+++ Lecture 43(Prototypes)/Prototype.js
@@ -0,0 +1,8 @@
+// I want k variable m jahan value store h na osk sath aghr whitespaces b hain tu jb m print kro wo na aye...
+
+// Example:
+
+let myname = "noor";
+let myname2 = "fatima";
+
+console.log(myname.length);

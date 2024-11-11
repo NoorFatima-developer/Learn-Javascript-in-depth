9711afc8432ac6a8c0f5004d84549b0756c37b3d
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-12 03:07:07 +0500

    code is added

--- Lecture 47_Lecture 48/Lecture 48(Getter & Setters)/notes.md
+++ Lecture 47_Lecture 48/Lecture 48(Getter & Setters)/notes.md
@@ -1,3 +1,5 @@
 ## Why and where we use getter and setter?
 
-<!-- Let say m kisi jagah pr apna password encrypted rkhna chah ri o or m chahti o wo secure rkhy m kisi ko b access na don k wo cher char kry tu osklye i will use getter & setter. -->
+<!-- Let say m kisi jagah pr apna password encrypted rkhna chah ri o or m chahti o wo secure rkhy m kisi ko b access na don k wo cher char kry tu osklye i will use getter & setter. ta k m kisi ko password deny ki bjye error dedo... -->
+
+## And 1 or important bt ye h k setter and getter hum hmesha ikathy hi use krygy...
--- Lecture 47_Lecture 48/Lecture 48(Getter & Setters)/setter_getter.js
+++ Lecture 47_Lecture 48/Lecture 48(Getter & Setters)/setter_getter.js
@@ -3,13 +3,33 @@ class User {
         this.email = email;
         this.password = password;
     }
+
+    // ye oper jo email and password hain ye hmry variables haina tu hum inmai sy kisi pr b getter and setter laga skty hain...
+    // behaves like method:
+
+    // get ko hum return krty hain..
+    get email() {
+        return this._email.toUpperCase();
+    }
+
+    // setters ko hum return nahi krty..
+    set email(value) {
+        this._email = value;
+    }
+
+    get password() {
+        return `${this._password}hitesh`;
+    }
+
+    set password(value) {
+        this._password = value.toUpperCase();
+}
+
 }
 
 console.log(User);
-
 // Create a new instance of the User class
-
-const noor = new User("noorfatima@gmail", "123");
+const noor = new User("noorfatima@gmail", "abc");
 console.log(noor.email, noor.password);
 
 

8dde95c3d647ee3fa86b04c9605efc36ae3d74bc
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-11 05:17:27 +0500

    code is added

--- Lecture 43(Prototypes)/Prototype.js
+++ Lecture 43(Prototypes)/Prototype.js
@@ -2,7 +2,11 @@
 
 // Example:
 
-let myname = "noor";
+let myname = "noor   ";
 let myname2 = "fatima";
 
-console.log(myname.length);
+console.log(myname.length); // console mai ye 7 dera hai output ku k ye whitespaces ko b add kr rha hai...
+console.log(myname.trim().length); // console mai ye 5 dera hai output ku k ye whitespaces ko  remove kr rha hai trim()...
+
+// Lkin problem ye h k mery ps jb alot of trims hongy tu tb kea m itny sary trims use krogi??
+// Nahi na tu what i will do now?

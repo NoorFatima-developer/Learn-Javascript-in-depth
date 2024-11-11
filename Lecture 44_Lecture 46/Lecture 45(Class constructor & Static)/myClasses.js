c1ec42f17356dbf9a3aef682d9b5af637dc6bf1c
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-11 22:01:33 +0500

    code is added

--- Lecture 44_Lecture 46/Lecture 45(Class constructor & Static)/myClasses.js
+++ Lecture 44_Lecture 46/Lecture 45(Class constructor & Static)/myClasses.js
@@ -5,13 +5,57 @@
 
 // ==========Create Classes in Jvascript Example:===========
 
+
+// Do this work by class..
 class User {
     constructor(username, email, password) {
         this.username = username;
         this.email = email;
         this.password = password;
     }
+
+    // Matlab m aghr ko b email password wagaira ko change krna chahti o ya kuch extra add tu osklye m new func bana k osmai add krogi...
+    // Mai password ko encrypt krk dena chahti hon direct essy nahi dena chahti islye i will do this:
+
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+
+    changeusername(){
+        return `${this.username.toUpperCase()}`
+    }
 }
 
-const chai = new User("chai", "chai@gmail.com");
+const chai = new User("chai", "chai@gmail.com", "123");
 console.log(chai);
+
+console.log(chai.encryptPassword());
+console.log(chai.changeusername());
+
+
+// Do above work by function..
+
+// Behind the scene kea kam horha hai discuss krlety hain wo b...
+// User basically hai tu function lki behind the scene ye "object ki trhan hi tu behave krta hai..."
+
+function User1(username,email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+}
+
+User1.prototype.encryptPassword1 = function(){
+    return `${this.password}abc`
+}
+User1.prototype.changeusername1 = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User1("tea", "tea@gmail.com", "123");
+console.log(tea);
+
+console.log(tea.encryptPassword1());
+console.log(tea.changeusername1());
+
+

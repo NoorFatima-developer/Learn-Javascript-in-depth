dce3abaad50f49260dded6de0c4ee79c51c4522e
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-11 21:26:49 +0500

    code is added

--- Lecture 44_Lecture 46/Lecture 45(Class constructor & Static)/myClasses.js
+++ Lecture 44_Lecture 46/Lecture 45(Class constructor & Static)/myClasses.js
@@ -2,3 +2,16 @@
 // Ye sari kahani hai after ES6 k....
 // Javascript m classes ka itni concept ni hota ku k whi sara kam hum prototypes sy krlety hain...
 // Lkin fer v we discuss kahani after ES6...
+
+// ==========Create Classes in Jvascript Example:===========
+
+class User {
+    constructor(username, email, password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
+
+const chai = new User("chai", "chai@gmail.com");
+console.log(chai);
